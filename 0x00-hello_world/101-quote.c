8e24b12245f3387387e1a5d1bc585d69e3008a24
yassinealaoui20 <yassinealaoui20@gmail.com>
2023-03-16 18:30:44 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line to the standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,");
+	fprintf(stderr, " 2015-10-19\n");
+	return (1);
+}
