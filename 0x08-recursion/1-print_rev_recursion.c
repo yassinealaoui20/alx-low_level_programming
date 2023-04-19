f266c90a88e9b802b5775e69fadcb1a852c1cea9
yassinealaoui20 <yassinealaoui20@gmail.com>
2023-04-18 23:21:08 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string follwed by '\n'
+ * @s: a pointer to the first C in array in memory
+ *
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursio--- - 1);
+		_putchar(*s);
+	}
+}
+
