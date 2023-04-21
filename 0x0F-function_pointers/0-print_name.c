58fc25d3dfa618d8d048f417c5455cb3e0923278
yassinealaoui20 <yassinealaoui20@gmail.com>
2023-04-21 15:47:47 -0700

    0-print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+
 
 /**
- * print_name -Prints a name
+ * print_name - Prints a name
  *
  * @name: Input
  * @f: Pointer
@@ -10,6 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL && name != NULL)
+	if (name && f)
 		f(name);
 }
