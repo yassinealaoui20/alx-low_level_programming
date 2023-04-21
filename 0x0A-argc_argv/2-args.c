cfd9dce7a64f4f9eeea2b1bbed4fb0e02363a215
yassinealaoui20 <yassinealaoui20@gmail.com>
2023-04-20 23:19:57 -0700

    2-atgs.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: an integer
+ * @argv: array pointers to char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int = 0;
+	while (i < argc)
+	{
+		printf("%S\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
