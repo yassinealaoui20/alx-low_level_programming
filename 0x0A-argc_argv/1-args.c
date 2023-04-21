6569c256f1db1f0e6041a659cf036cc89082b086
yassinealaoui20 <yassinealaoui20@gmail.com>
2023-04-20 23:02:34 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: an integer
+ * @argv: array pointers to char
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
