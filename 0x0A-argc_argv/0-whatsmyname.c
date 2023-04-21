13d031360ee5e13142fe8661d18c68f2ef433d50
yassinealaoui20 <yassinealaoui20@gmail.com>
2023-04-20 22:53:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: an integer
+ * @argv: array pointers to char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
