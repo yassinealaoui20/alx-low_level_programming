6dbad87c18f74913b64db62adad33fba04db2200
yassinealaoui20 <yassinealaoui20@gmail.com>
2024-01-29 06:46:38 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,9 +20,9 @@ int main(void)
 	if (n % 10 > 5)
 		printf(" is %d and is greater than 5\n", n % 10);
 	else if (n % 10 == 0)
-		printf(" is %d and is 0", n % 10);
+		printf(" is %d and is 0\n", n % 10);
 	else
-		printf(" is %d and is less than 6 and not 0", n % 10);
+		printf(" is %d and is less than 6 and not 0\n", n % 10);
 
 	return (0);
 }
